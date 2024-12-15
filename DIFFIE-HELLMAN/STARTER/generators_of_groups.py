429b5a70af79973067ec944f1ae1a6bb1b5e782e
Poornamol Vettiparampil Damodaran <108222742+B00177274@users.noreply.github.com>
2024-12-15 11:16:28 +0000

    Update generators_of_groups.py

--- DIFFIE-HELLMAN/STARTER/generators_of_groups.py
+++ DIFFIE-HELLMAN/STARTER/generators_of_groups.py
@@ -9,3 +9,16 @@ for k in range(p):
   if is_generator(k, p):
     print(k)
     break
+
+
+
+#solution
+#This code searches for a primitive root modulo a prime number 
+#𝑝
+#p. It defines a function that checks if a given number 
+#𝑘
+#k is a generator by calculating its powers modulo 
+#𝑝
+#p and ensuring that none of them repeat prematurely. The loop iterates through possible values of 
+#𝑘
+#k to find the first number that satisfies the condition of being a generator. If a generator is found, the program prints the value and stops searching.
