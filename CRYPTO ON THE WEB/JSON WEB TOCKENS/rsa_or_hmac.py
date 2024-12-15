e1c846cbdcd90103825fa8135df000248b16b42f
Poornamol Vettiparampil Damodaran <108222742+B00177274@users.noreply.github.com>
2024-12-15 11:04:07 +0000

    Update rsa_or_hmac.py

--- CRYPTO ON THE WEB/JSON WEB TOCKENS/rsa_or_hmac.py
+++ CRYPTO ON THE WEB/JSON WEB TOCKENS/rsa_or_hmac.py
@@ -23,3 +23,8 @@ print("Evil token", evil_token)
 
 r = requests.get(get_auth_route(evil_token))
 print(r.content.decode("utf-8"))
+
+#solution
+#This script demonstrates a JWT vulnerability by using an RSA public key in an unintended way to create a malicious token. 
+#It retrieves the public key and uses the PyJWT library to encode a token, but signs it with the HS256 algorithm, instead of using the correct RSA signature. 
+#The token is then sent to the authorization endpoint, attempting to bypass the security by impersonating an admin.
